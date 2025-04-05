f2d02c7f098e8ab33ec19ceaa4239ec538af6b17
조승욱 <ori@ori-dev.com>
2025-04-05 12:16:46 +0900

    0405_02

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -4,7 +4,7 @@ import { Canvas } from '@react-three/fiber'
 import React, { Suspense } from 'react'
 import Model from './Model'
 import { Center, OrbitControls, Text } from '@react-three/drei'
-import RayExample from './RayExample'
+import MainLoading from './MainLoading'
 
 function Scene() {
   return (
@@ -12,7 +12,7 @@ function Scene() {
     <Canvas camera={{ position: [0, 20, 50], fov: 60 }}>
 
         <directionalLight position={[5, 5, 5]} intensity={4} />
-        <Suspense fallback={null}>
+        <Suspense fallback={<MainLoading />}>
             <Center>
                 <Model />
                 {/* <RayExample /> */}
